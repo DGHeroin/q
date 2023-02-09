1a8d8d72063670339a130e212fe7d1d2d77728c4
DGHeroin <DGHeroin@gmail.com>
2023-02-09 16:39:51 +0800

    优化H接口

--- h.go
+++ h.go
@@ -2,381 +2,298 @@ package q
 
 import (
     "encoding/json"
+    "errors"
     "fmt"
-    "reflect"
     "strconv"
     "strings"
+    "time"
 )
 
-type (
-    H  map[string]interface{}
-    Hs []H
-)
+type H map[string]interface{}
+type Hs []H
 
-func (h H) GetFloat64(key string) (float64, bool) {
-    v, ok := h[key]
-    if !ok {
-        return 0, false
-    }
-    str := fmt.Sprintf("%v", v)
-    f64, err := strconv.ParseFloat(str, 10)
-    if err != nil {
-        return 0, false
-    }
-    return f64, true
-}
-func (h H) GetInt64(key string) (int64, bool) {
-    v, ok := h[key]
-    if !ok {
-        return 0, false
-    }
-    str := fmt.Sprintf("%v", v)
-    f64, err := strconv.ParseFloat(str, 10)
-    if err != nil {
-        return 0, false
-    }
-    return int64(f64), true
-}
-func (h H) GetInt(key string) (int, bool) {
-    v, ok := h.GetInt64(key)
-    return int(v), ok
-}
-func (h H) GetBool(key string) (bool, bool) {
-    v, ok := h[key]
-    if !ok {
-        return false, false
-    }
-    str := fmt.Sprintf("%v", v)
-    if str == "true" {
-        return true, true
-    } else if str == "false" {
-        return false, true
-    }
-    return false, false
-}
-func (h H) GetString(key string) (string, bool) {
+func (h H) ToJson() string {
     if h == nil {
-        return "", false
+        return ""
     }
-    v, ok := h[key]
-    if !ok {
-        return "", false
+    data, _ := json.Marshal(&h)
+    return string(data)
+}
+func (h H) ToJsonPretty() string {
+    if h == nil {
+        return ""
     }
-    return fmt.Sprintf("%v", v), true
+    data, _ := json.MarshalIndent(&h, "", "  ")
+    return string(data)
 }
 
-func (h H) GetIntArray(key string) (result []int, ok bool) {
-    str := ""
-    obj := reflect.TypeOf(h[key])
-    if obj == nil {
-        return nil, false
+func (h Hs) ToJson() string {
+    if h == nil {
+        return ""
     }
-    switch obj.Kind() {
-    case reflect.Float64:
-        f := h[key].(float64)
-        str = fmt.Sprintf("%d", int64(f))
-    default:
-        str = fmt.Sprint(h[key])
-    }
-    tokens := strings.Split(str, ",")
-
-    for _, tok := range tokens {
-        if tok != "" {
-            val, _ := strconv.Atoi(tok)
-            result = append(result, val)
-        }
-    }
-
-    return result, true
+    data, _ := json.Marshal(&h)
+    return string(data)
 }
-func (h H) GetInt64Array(key string) (result []int64, ok bool) {
-    str := ""
-    obj := reflect.TypeOf(h[key])
-    if obj == nil {
-        return nil, false
+func (h Hs) ToJsonPretty() string {
+    if h == nil {
+        return ""
     }
-    switch obj.Kind() {
-    case reflect.Float64:
-        f := h[key].(float64)
-        str = fmt.Sprintf("%d", int64(f))
-    default:
-        str = fmt.Sprint(h[key])
-    }
-    tokens := strings.Split(str, ",")
-
-    for _, tok := range tokens {
-        if tok != "" {
-            val, _ := strconv.ParseInt(tok, 10, 64)
-            result = append(result, val)
-        }
-    }
-
-    return result, true
-}
-func (h H) GetSingleIntArray(key string) (result []int, ok bool) {
-    str, _ := h.GetString(key)
-    tokens := strings.Split(str, "|")
-
-    for _, tok := range tokens {
-        if tok != "" {
-            val, _ := strconv.Atoi(tok)
-            result = append(result, val)
-        }
-    }
-
-    return result, true
-}
-func (h H) GetSingleInt64Array(key string) (result []int64, ok bool) {
-    str, _ := h.GetString(key)
-    tokens := strings.Split(str, "|")
-
-    for _, tok := range tokens {
-        if tok != "" {
-            val, _ := strconv.ParseInt(tok, 10, 64)
-            result = append(result, val)
-        }
-    }
-
-    return result, true
+    data, _ := json.MarshalIndent(&h, "", "  ")
+    return string(data)
 }
 
-func (h H) Float64(key string) float64 {
-    v, ok := h[key]
-    if !ok {
-        return 0
+func (h H) String(key string) string {
+    var p map[string]interface{} = h
+    v, err := getNestedValue(p, key, ".")
+    if err != nil {
+        return ""
     }
-    str := fmt.Sprintf("%v", v)
-    f64, err := strconv.ParseFloat(str, 10)
+    if str, ok := v.(string); ok {
+        return str
+    }
+    return ""
+}
+func (h H) Int(key string) int64 {
+    var p map[string]interface{} = h
+    v, err := getNestedValue(p, key, ".")
     if err != nil {
         return 0
     }
-    return f64
-}
-func (h H) Int64(key string) int64 {
-    v, ok := h[key]
-    if !ok {
-        return 0
+    var val int64
+    switch vv := v.(type) {
+    case float64:
+        val = int64(vv)
+    default:
+        val, _ = strconv.ParseInt(fmt.Sprint(v), 10, 64)
     }
-    str := fmt.Sprintf("%v", v)
-    f64, err := strconv.ParseFloat(str, 10)
+    return val
+}
+func (h H) Float(key string) float64 {
+    var p map[string]interface{} = h
+    v, err := getNestedValue(p, key, ".")
     if err != nil {
         return 0
     }
-    return int64(f64)
-}
-func (h H) Int(key string) int {
-    v, _ := h.GetInt64(key)
-    return int(v)
+    val, _ := strconv.ParseFloat(fmt.Sprint(v), 64)
+    return val
 }
 func (h H) Bool(key string) bool {
-    v, ok := h[key]
-    if !ok {
+    var p map[string]interface{} = h
+    v, err := getNestedValue(p, key, ".")
+    if err != nil {
         return false
     }
-    str := fmt.Sprintf("%v", v)
+    str := strings.ToLower(fmt.Sprint(v))
     if str == "true" {
         return true
-    } else if str == "false" {
-        return false
+    }
+    if str == "1" {
+        return true
     }
     return false
 }
-func (h H) String(key string) string {
-    if h == nil {
-        return ""
+func (h H) UnixTime(key string) time.Time {
+    sec := h.Int(key)
+    return time.Unix(sec, 0)
+}
+func (h H) UnixTimeString(key string) string {
+    sec := h.Int(key)
+    return time.Unix(sec, 0).Format("2006-01-02 15:04:05.000 Z07:00")
+}
+func (h H) ByteSize(key string) string {
+    v := h.Float(key)
+    units := "B"
+    if v > 1024 {
+        v = v / 1024
+        units = "K"
     }
-    v, ok := h[key]
-    if !ok {
-        return ""
+    if v > 1024 {
+        v = v / 1024
+        units = "M"
     }
+    if v > 1024 {
+        v = v / 1024
+        units = "G"
+    }
+    if v > 1024 {
+        v = v / 1024
+        units = "T"
+    }
+    v, _ = strconv.ParseFloat(fmt.Sprintf("%.2f", v), 64)
+    return fmt.Sprintf("%g%s", v, units)
+}
+func (h H) H(key string) H {
+    var p map[string]interface{} = h
+    v, err := getNestedValue(p, key, ".")
+    if err != nil {
+        return nil
+    }
+    tmp, ok := v.(map[string]interface{})
+    if ok {
+        return tmp
+    }
+    return nil
+}
+func (h H) Hs(key string) Hs {
+    var p map[string]interface{} = h
+    v, err := getNestedValue(p, key, ".")
+    if err != nil {
+        return nil
+    }
+    var result Hs
+    if tmp, ok := v.([]interface{}); ok {
+        for _, v := range tmp {
+            if h, ok := v.(map[string]interface{}); ok {
+                result = append(result, h)
+            }
+        }
+    }
+    return result
+}
+
+var (
+// empty interface{}
+)
+
+func toFloat(v interface{}) (float64, bool) {
+    switch u := v.(type) {
+    case int:
+        return float64(u), true
+    case int8:
+        return float64(u), true
+    case int16:
+        return float64(u), true
+    case int32:
+        return float64(u), true
+    case int64:
+        return float64(u), true
+    case uint:
+        return float64(u), true
+    case uint8:
+        return float64(u), true
+    case uint16:
+        return float64(u), true
+    case uint32:
+        return float64(u), true
+    case uint64:
+        return float64(u), true
+    case uintptr:
+        return float64(u), true
+    case float32:
+        return float64(u), true
+    case float64:
+        return u, true
+    default:
+        return 0, false
+    }
+}
+func getNestedValue(input interface{}, node, separator string) (interface{}, error) {
+    tokens := strings.Split(node, separator)
+    for _, n := range tokens {
+        if isIndex(n) {
+            // find slice/array
+            if arr, ok := input.([]interface{}); ok {
+                idx, err := getIndex(n)
+                if err != nil {
+                    return input, err
+                }
+                arrLen := len(arr)
+                if arrLen == 0 ||
+                    idx > arrLen-1 {
+                    return empty, errors.New("empty array")
+                }
+                input = arr[idx]
+            }
+        } else {
+            // find in map
+            validNode := false
+            if mp, ok := input.(map[string]interface{}); ok {
+                input, ok = mp[n]
+                validNode = ok
+            } else {
+                if mp, ok := input.(H); ok {
+                    input, ok = mp[n]
+                    validNode = ok
+                }
+            }
+
+            // find in group data
+            if mp, ok := input.(map[string][]interface{}); ok {
+                input, ok = mp[n]
+                validNode = ok
+            }
+
+            if !validNode {
+                return empty, fmt.Errorf("invalid node name %s", n)
+            }
+        }
+    }
+
+    return input, nil
+}
+func isIndex(in string) bool {
+    return strings.HasPrefix(in, "[") && strings.HasSuffix(in, "]")
+}
+func getIndex(in string) (int, error) {
+    if !isIndex(in) {
+        return -1, fmt.Errorf("invalid index")
+    }
+    is := strings.TrimLeft(in, "[")
+    is = strings.TrimRight(is, "]")
+    val, err := strconv.Atoi(is)
+    if err != nil {
+        return -1, err
+    }
+    return val, nil
+}
+func toString(v interface{}) string {
     return fmt.Sprintf("%v", v)
 }
-
-func (h H) StringArray(key string) []string {
-    var res []string
-    if h == nil {
-        return res
+func toInt(v interface{}) (int, bool) {
+    if val, ok := toInt64(v); ok {
+        return int(val), true
     }
-
-    str, ok := h.GetString(key)
-    if !ok {
-        return res
-    }
-
-    tokens := strings.Split(str, ",")
-    for _, token := range tokens {
-        res = append(res, token)
-    }
-
-    return res
+    return 0, false
 }
-func (h H) ParseTimeN() (result [][2]int64) {
-    if n, ok := h.GetInt("TimeN"); ok {
-        for i := 1; i <= n; i++ {
-            startTime, ok1 := h.GetInt64(fmt.Sprintf("StartTime%d", i))
-            stopTime, ok2 := h.GetInt64(fmt.Sprintf("StopTime%d", i))
-            if ok1 && ok2 {
-                result = append(result, [2]int64{startTime, stopTime})
-            }
-        }
-    }
-    return
-}
-func (h H) IntArray(key string) (result []int) {
-    str := ""
-    obj := reflect.TypeOf(h[key])
-    if obj == nil {
-        return nil
-    }
-    switch obj.Kind() {
-    case reflect.Float64:
-        f := h[key].(float64)
-        str = fmt.Sprintf("%d", int64(f))
+func toInt64(v interface{}) (int64, bool) {
+    switch u := v.(type) {
+    case int:
+        return int64(u), true
+    case int8:
+        return int64(u), true
+    case int16:
+        return int64(u), true
+    case int32:
+        return int64(u), true
+    case int64:
+        return u, true
+    case uint:
+        return int64(u), true
+    case uint8:
+        return int64(u), true
+    case uint16:
+        return int64(u), true
+    case uint32:
+        return int64(u), true
+    case uint64:
+        return int64(u), true
+    case uintptr:
+        return int64(u), true
+    case float32:
+        return int64(u), true
+    case float64:
+        return int64(u), true
     default:
-        str = fmt.Sprint(h[key])
+        return 0, false
     }
-    tokens := strings.Split(str, ",")
-
-    for _, tok := range tokens {
-        if tok != "" {
-            val, _ := strconv.Atoi(tok)
-            result = append(result, val)
-        }
-    }
-    return result
 }
-func (h H) Int64Array(key string) (result []int64) {
-    str := ""
-    obj := reflect.TypeOf(h[key])
-    if obj == nil {
-        return nil
-    }
-    switch obj.Kind() {
-    case reflect.Float64:
-        f := h[key].(float64)
-        str = fmt.Sprintf("%d", int64(f))
+func isNumber(v interface{}) bool {
+    switch v.(type) {
+    case int, int8, int16, int32, int64, uint, uint8, uint16, uint32, uint64, uintptr:
+        return true
+    case float32, float64:
+        return true
     default:
-        str = fmt.Sprint(h[key])
-    }
-    tokens := strings.Split(str, ",")
-
-    for _, tok := range tokens {
-        if tok != "" {
-            val, _ := strconv.ParseInt(tok, 10, 64)
-            result = append(result, val)
-        }
-    }
-
-    return result
-}
-func (h H) SingleIntArray(key string) (result []int) {
-    str, _ := h.GetString(key)
-    tokens := strings.Split(str, "|")
-
-    for _, tok := range tokens {
-        if tok != "" {
-            val, _ := strconv.Atoi(tok)
-            result = append(result, val)
-        }
-    }
-
-    return result
-}
-func (h H) SingleInt64Array(key string) (result []int64) {
-    str, _ := h.GetString(key)
-    tokens := strings.Split(str, "|")
-
-    for _, tok := range tokens {
-        if tok != "" {
-            val, _ := strconv.ParseInt(tok, 10, 64)
-            result = append(result, val)
-        }
-    }
-
-    return result
-}
-
-func (h H) IntSlice(key string) (result []int) {
-    p := h.Get(key)
-    if p == nil {
-        return
-    }
-    if arr, ok := p.([]interface{}); ok {
-        for _, i := range arr {
-            if val, ok := toInt(i); ok {
-                result = append(result, val)
-            }
-        }
-    }
-    return
-}
-func (h H) ToJson() string {
-    if data, err := json.Marshal(&h); err == nil {
-        return string(data)
-    }
-    return "{}"
-}
-func (h H) HasKey(key string) bool {
-    if h == nil {
         return false
     }
-    _, ok := h[key]
-    return ok
-}
-func (h H) Get(s string) interface{} {
-    if h == nil {
-        return nil
-    }
-    return h[s]
-}
-func (h H) GetMap(s string) (H, bool) {
-    if h == nil {
-        return nil, false
-    }
-    obj, ok := h[s]
-    if !ok {
-        return nil, false
-    }
-
-    r, ok := obj.(map[string]interface{})
-    return r, ok
-}
-func (h H) TryGet(s string) (interface{}, bool) {
-    if h == nil {
-        return nil, false
-    }
-    r, ok := h[s]
-    return r, ok
-}
-func (h H) ContainsStringWithSplit(key string, s string, sep string) bool {
-    if !h.HasKey(key) {
-        return false
-    }
-    str, _ := h.GetString(key)
-    tokens := strings.Split(str, sep)
-    for _, token := range tokens {
-        if s == token {
-            return true
-        }
-    }
-    return false
-}
-func (h H) ToQ() *Q {
-    return NewWithString(h.ToJson())
-}
-
-func (hs Hs) GetInt64(line int, key string) (int64, bool) {
-    h := hs[line]
-    return h.GetInt64(key)
-}
-func (hs Hs) GetInt(line int, key string) (int, bool) {
-    h := hs[line]
-    return h.GetInt(key)
-}
-func (hs Hs) GetString(line int, key string) (string, bool) {
-    h := hs[line]
-    return h.GetString(key)
-}
-func (hs Hs) Len() int {
-    return len(hs)
 }
